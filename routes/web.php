812337fd3fad61c59c0629367d5ddc9778e38579
JenhLi252 <lizanovillalobosjenhson@gmail.com>
2022-12-04 09:57:18 -0600

    login and register

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->middleware('auth');
+
+Route::get('/register', [RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register.index');
+
+Route::post('/register', [RegisterController::class, 'store'])
+    ->name('register.store');
+
+
+
+Route::get('/login', [SessionController::class, 'create'])
+    ->middleware('guest')
+    ->name('login.index');
+
+Route::post('/login', [SessionController::class, 'store'])
+    ->name('login.store');
+
+Route::get('/logout', [SessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('login.destroy');
