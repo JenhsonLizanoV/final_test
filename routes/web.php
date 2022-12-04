7b0f9466883d49fb2ad5a5ca105344be10007464
JenhLi252 <lizanovillalobosjenhson@gmail.com>
2022-12-04 10:56:56 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 
-//Routes
+
 Route::get('/', function () {
     return view('home');
 })->middleware('auth');
